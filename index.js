af9c44e281a477fc918c73e4af59d6457b1c62ff
Maxieck98 <chrismaxwels98@gmail.com>
2022-07-15 14:16:56 +0300

    javascript in head part of html

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 //js functions
-document.getElementById('demo').innerHTML = 'Hello JavaScript';
-document.getElementById("demo").style.display = "none";
+//document.getElementById("demo").innerHTML = "My First JavaScript";
